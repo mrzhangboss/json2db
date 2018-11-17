00bf8559ad4075bbc6af3924798b9c526ecd7950
mrzhangboss <2529450174@qq.com>
2018-11-17 14:17:05 +0800

    :star: add upload command

---
+++ README.md
@@ -0,0 +1,8 @@
+# Json2DB
+
+> Python3.6+
+
+
+Just need defined a json object, then you can save it in to a relational DB like MySQL PostgreSQL or other DB.
+
+## Reason To Write This FrameWork
---
+++ setup.py
@@ -0,0 +1,77 @@
+# -*- coding:utf-8 -*-
+import sys
+import os
+import setuptools
+from shutil import rmtree
+from setuptools import Command, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+VERSION = "0.0.2"
+REQUIRED = [
+
+]
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Publishing git tags…')
+        os.system('git tag v{0}'.format(VERSION))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+setup(
+    name="json2db",
+    version=VERSION,
+    author="mrzhangboss",
+    author_email="2529450174@qq.com",
+    description="convert json to relational db",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mrzhangboss/json2db",
+    py_modules=['json2db'],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6.0",
+    requires=REQUIRED,
+    cmdclass={
+        'upload': UploadCommand,
+    },
+)
