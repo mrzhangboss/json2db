ecc779b5fa5f189ae29b9d77f5cc6005e6ec9e7e
mrzhangboss <2529450174@qq.com>
2018-12-05 22:41:27 +0800

    :tuda: v3.1.2
    - add convert function ready for model migrations

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import Command, setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-VERSION = "3.1.0"
+VERSION = "3.1.2"
 REQUIRED = [
     "sqlalchemy", "attrs", "python-dateutil"
 
